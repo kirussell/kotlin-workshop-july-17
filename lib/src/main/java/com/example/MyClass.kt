006f7d61fbc5b77592c1e388d313e49b5cac1510
kirussell <ruslan.kim@grabtaxi.com>
2017-07-13 02:00:07 +0800

    Returns Json with list of members

--- lib/src/main/java/com/example/MyClass.kt
+++ lib/src/main/java/com/example/MyClass.kt
@@ -1,7 +1,31 @@
 package com.example
 
+import com.google.gson.Gson
 import spark.Spark
 
+val dao = MembersDao()
+val gson = Gson()
+
+fun Any?.toJson(): String = gson.toJson(this)
+
 fun main(args: Array<String>) {
+    helloWorld()
+    Spark.get("kotlin/members") { _, _ -> dao.all().toJson() }
+}
+
+private fun helloWorld() {
     Spark.get("/kotlin/hello") { _, _ -> "Hello Kotlin User Group!" }
+}
+
+data class Member(val name: String, val paid: Int)
+
+class MembersDao {
+    fun all() = javaClass.classLoader
+            .getResource("members.db")
+            .readText()
+            .reader()
+            .buffered()
+            .readLines()
+            .map { it.split(" ") }
+            .map { Member(it[0], it[1].toInt()) }
 }
